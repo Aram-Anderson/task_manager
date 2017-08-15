16fffb010c5050ecc4ccd0508fb752d43cddea1a
Aram Anderson <aram.j.anderson@gmail.com>
2017-08-14 21:52:58 -0600

    Additions working

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -3,6 +3,7 @@ require 'pry'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -33,6 +34,21 @@ class TaskManagerApp < Sinatra::Base
   post '/tasks' do
     task = Task.new(params[:task])
     task.save
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
     redirect '/tasks'
   end
 end
